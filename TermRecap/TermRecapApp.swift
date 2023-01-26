8311728030d0d8332bb4e17f77642c2fb77c69d8
Tom <twu@lcs.on.ca>
2023-01-26 09:35:43 -0500

    Initial Commit

---
+++ TermRecap/TermRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermRecapApp.swift
+//  TermRecap
+//
+//  Created by Tom Wu on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TermRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
